8117e30ce09dfda61fa252f78b75cd18b3a0155f
Georg Ringer <georg.ringer@gmail.com>
2024-03-04 22:38:20 +0100

    cart clearing + email

--- Classes/Controller/Order/PaymentController.php
+++ Classes/Controller/Order/PaymentController.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace GeorgRinger\CartStripe\Controller\Order;
 
 use Extcode\Cart\Domain\Model\Cart;
+use Extcode\Cart\Domain\Model\Order\Item;
 use Extcode\Cart\Domain\Repository\CartRepository;
 use Extcode\Cart\Domain\Repository\Order\PaymentRepository;
+use Extcode\Cart\Service\MailHandler;
 use Extcode\Cart\Service\SessionHandler;
+use Extcode\Cart\Utility\CartUtility;
 use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Log\LogManagerInterface;
@@ -51,7 +54,8 @@ class PaymentController extends ActionController
         PersistenceManager $persistenceManager,
         SessionHandler $sessionHandler,
         CartRepository $cartRepository,
-        PaymentRepository $paymentRepository
+        PaymentRepository $paymentRepository,
+        CartUtility $cartUtility
     )
     {
         $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
@@ -59,6 +63,7 @@ class PaymentController extends ActionController
         $this->sessionHandler = $sessionHandler;
         $this->cartRepository = $cartRepository;
         $this->paymentRepository = $paymentRepository;
+        $this->cartUtility = $cartUtility;
     }
 
     protected function initializeAction(): void
@@ -78,15 +83,28 @@ class PaymentController extends ActionController
 
     public function successAction(): void
     {
-        DebuggerUtility::var_dump(GeneralUtility::_GET());
-        DebuggerUtility::var_dump(GeneralUtility::_POST());
-        DebuggerUtility::var_dump($this->request->getArguments());
-
+//        DebuggerUtility::var_dump(GeneralUtility::_POST());
+//        DebuggerUtility::var_dump($this->request->getArguments());
+//die('xxx');
         if ($this->request->hasArgument('hash') && !empty($this->request->getArgument('hash'))) {
             $this->loadCartByHash($this->request->getArgument('hash'));
 
             if ($this->cart) {
                 $orderItem = $this->cart->getOrderItem();
+                if ($orderItem) {
+
+                    $payment = $orderItem->getPayment();
+
+                    if ($payment->getStatus() !== 'paid') {
+                        $payment->setStatus('paid');
+                        $this->paymentRepository->update($payment);
+                        $this->persistenceManager->persistAll();
+
+
+                        $this->notify($orderItem);
+                        $this->clearCart($orderItem->getPid());
+                    }
+                }
 
                 $this->redirect('show', 'Cart\Order', 'Cart', ['orderItem' => $orderItem]);
             } else {
@@ -111,6 +129,54 @@ class PaymentController extends ActionController
                 AbstractMessage::ERROR
             );
         }
+    }
+
+    protected function notify(Item $orderItem): void
+    {
+        $this->sendBuyerMail($orderItem);
+        $this->sendSellerMail($orderItem);
+    }
+
+    protected function clearCart(int $pid)
+    {
+        $this->sessionHandler->clear($pid);
+
+        $GLOBALS['TSFE']->fe_user->setKey('ses', 'cart_billing_address_' . $pid, null);
+        $GLOBALS['TSFE']->fe_user->setKey('ses', 'cart_shipping_address_' . $pid, null);
+        $GLOBALS['TSFE']->fe_user->storeSessionData();
+        die('xxclx');
+    }
+
+    /**
+     * Send a Mail to Buyer
+     *
+     * @param Item $orderItem
+     */
+    protected function sendBuyerMail(
+        Item $orderItem
+    )
+    {
+        $mailHandler = GeneralUtility::makeInstance(
+            MailHandler::class
+        );
+        $mailHandler->setCart($this->cart->getCart());
+        $mailHandler->sendBuyerMail($orderItem);
+    }
+
+    /**
+     * Send a Mail to Seller
+     *
+     * @param Item $orderItem
+     */
+    protected function sendSellerMail(
+        Item $orderItem
+    )
+    {
+        $mailHandler = GeneralUtility::makeInstance(
+            MailHandler::class
+        );
+        $mailHandler->setCart($this->cart->getCart());
+        $mailHandler->sendSellerMail($orderItem);
     }
 
     public function cancelAction(): void
